eb3d4516b72c05eefaf9ec970eadc682d923bdde
alexandrethm <33730312+alexandrethm@users.noreply.github.com>
2019-03-05 19:52:25 +0100

    Updates

---
+++ precache-manifest.ced0196b6869a2463b0d939a05f28f0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "bd4de7ae32d74b318af5",
+    "url": "/blindtest-spotify/static/js/runtime~main.bd4de7ae.js"
+  },
+  {
+    "revision": "6d773b51d84261251e7a",
+    "url": "/blindtest-spotify/static/js/main.6d773b51.chunk.js"
+  },
+  {
+    "revision": "3f7525847cbf7fd2fe0c",
+    "url": "/blindtest-spotify/static/js/1.3f752584.chunk.js"
+  },
+  {
+    "revision": "6d773b51d84261251e7a",
+    "url": "/blindtest-spotify/static/css/main.e0b94752.chunk.css"
+  },
+  {
+    "revision": "843c275e4edbb5e65c8f719e092df686",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
